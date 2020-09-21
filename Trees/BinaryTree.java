7522915360d18ec841a248f17d36086d3c2ac892
Keefer Gregoire <keefergregoire@Keefers-MacBook-Pro.local>
2020-09-21 13:18:52 -0400

    Delete element implemented

--- Trees/BinaryTree.java
+++ Trees/BinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Main {
   public static void main(String[] args) {
     int[] data = {5,2,6,7,10,12};
@@ -5,7 +8,7 @@ class Main {
     for(int i =0; i< data.length; i++){
       tree.insertElement(data[i]);
     }
-
+    tree.deleteElement(7);
     tree.printInOrder(tree.getRoot());
   }
 }
@@ -107,11 +110,107 @@ class BinaryTree{
       return node;
   }
 
-  public void deleteElement(){
-      
+  public void deleteElement(int data){
+      if(root ==null){
+        return;
+      }
+
+      if(this.root.getLeft() == null && this.root.getRight() == null){
+        if(root.getData() == data){
+          return;
+        }else{
+          return;
+        }
+      }
+
+      Queue<BinaryTreeNode> q = new LinkedList<BinaryTreeNode>();
+      q.add(this.root);
+      BinaryTreeNode temp = new BinaryTreeNode(), dataNode = new BinaryTreeNode();
+
+      while(!q.isEmpty()){
+        temp = q.peek();
+        q.remove();
+
+        if(temp.getData() == data){
+            dataNode = temp;
+        }
+
+        if(temp.getLeft() != null){
+          q.add(temp.getLeft());
+        }
+
+        if(temp.getRight() != null){
+          q.add(temp.getRight());
+        }
+      }
+
+      if(dataNode != null){
+
+        int x = temp.getData();
+        deleteDeepestNode(this.root, temp);
+        dataNode.setData(x);
+      }
   }
 
-  public void findElement(){
+  private void deleteDeepestNode(BinaryTreeNode root, BinaryTreeNode delNode){
+    Queue<BinaryTreeNode> q = new LinkedList<BinaryTreeNode>();
+    q.add(root);
+    BinaryTreeNode temp = null;
 
+    while(!q.isEmpty()){
+      temp = q.peek();
+      q.remove();
+
+      if(temp == delNode){
+        temp = null;
+        return;
+      }
+
+      if(temp.getRight() != null){
+        if(temp.getRight() == delNode){
+          temp.setRight(null);
+          return;
+        }else{
+          q.add(temp.getRight());
+        }
+      }
+
+      if(temp.getLeft() != null){
+        if(temp.getLeft() == delNode){
+          temp.setLeft(null);
+          return;
+        }else{
+          q.add(temp.getLeft());
+        }
+      }
+    }
   }
+
+  public void findNode(int data){
+    
+  }
+
+  public BinaryTreeNode findElement(int data){
+      return findHelper(this.root, data);
+  }
+
+  private BinaryTreeNode findHelper(BinaryTreeNode node, int data){
+    if (node == null){
+      return node;
+    }
+
+    if(node.getData() == data){
+      return node;
+    }
+
+    BinaryTreeNode leftNode = findHelper(node.getLeft(), data);
+    if(leftNode!= null && leftNode.getData() == data){
+      return leftNode;
+    }
+
+    BinaryTreeNode rightNode = findHelper(node.getRight(), data);
+
+    return rightNode;
+  }
+
 }
